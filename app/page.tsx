c53d8b0d1f7f591d398aa0468c378370016f194b
mie035 <hello@michitoshiyano.com>
2024-07-22 00:30:20 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,7 @@
 import { Metadata } from "next";
 import Image from "next/image";
-import { PlusCircledIcon } from "@radix-ui/react-icons";
 
-import { Button } from "@/components/ui/button";
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
-import { Separator } from "@/components/ui/separator";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-
-import { AlbumArtwork } from "@/components/album-artwork";
-import { Menu } from "@/components/menu";
-import { PodcastEmptyPlaceholder } from "@/components/podcast-empty-placeholder";
 import { Sidebar } from "@/components/sidebar";
-import { listenNowAlbums, madeForYouAlbums } from "../data/albums";
 import { playlists } from "../data/playlists";
 
 export const metadata: Metadata = {
